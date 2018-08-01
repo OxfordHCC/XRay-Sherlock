29a3c0e4f787dc6a3ad38e230455b8e129079863
adam slack <adamslack@outlook.com>
2018-08-01 17:18:09 +0100

    queries

--- nodejs/adhoc/example.js
+++ nodejs/adhoc/example.js
@@ -2,13 +2,38 @@ const Sherlock = require('../sherlock/sherlock');
 
 class ExampleAdHoc extends Sherlock {
 
-    processAPKManifest() {
-        // Overrides parent method for processing APK Manifest.
+    /**
+     * Process APK Manifest.
+     *
+     * Overrides the parent process APK Manifest method.
+     *
+     * @param {Array of Strings, one for each line in the mainfest} mainfest 
+     * @returns {The results of analysis as a JSON object.}
+     */
+    analyseApp(mainfest, classFile, smali) {
 
-        let jsonResult = {
-            has_thing : true,
-            number_of_stuff : 9001
+        let hasThing = false;
+        let numberOfStuff = 0;
+
+        for(const line of classFile) {
+
+            if(line.includes("thing")) {
+                hasThing = true;
+            }
+
+            if(line.includes("stuff")) {
+                numberOfStuff += 1;
+            }
         }
+
+
+        // Build a JSON object of the results
+        let jsonResult = {
+            has_thing : hasThing,
+            number_of_stuff : numberOfStuff
+        }
+
+        // return the json object of results.
         return jsonResult
     }
 }
--- nodejs/sherlock/sherlock.js
+++ nodejs/sherlock/sherlock.js
@@ -1,4 +1,5 @@
-
+const config = require('../config/config.json');
+const pg = require('pg');
 
 class Sherlock {
 
@@ -13,58 +14,137 @@ class Sherlock {
     useDateRange;
     useAppPackageList;
     useLatestAppVerions;
-    
+
+    // Database connection pool
+    pool;
+
+    // Name of the Analyser. Used to distinguish between results in the DB.
+    analyserName;
+
     constructor(
+        analyserName,
         maxDate = new Date(),
         minDate = new Date(),
-        useMaxDate = false,
-        useMinDate = false,
-        useDateRange = false,
-        useAppPackageList = false,
         appPackageList = [],
         useLatestAppVerions = true
     ) {
+        // Must be set.
+        this.analyserName = analyserName
+
+        // optional Params
         this.maxDate = maxDate;
         this.minDate = minDate;
-        this.useMaxDate = useMaxDate;
-        this.useMinDate = useMinDate;
-        this.useDateRange = useDateRange;
-        this.useAppPackageList = useAppPackageList;
         this.appPackageList = appPackageList;
         this.useLatestAppVerions = useLatestAppVerions;
+
+        // DB setup
+        const dbConfig = config.db_info;
+        dbConfig.max = 10;
+        dbCfg.idleTimeoutMillis = 30000;
+
+        this.pool = new pg.Pool(dbConfig);
+        this.pool.on('error', (err) => {
+            console.log('idle client error', err.message, err.stack);
+        });
     }
 
-    // Query the DB for a set of APK names.
-    // Needs to be flexible, and allow condtions on what is retrieved.
-    getAppPackageNames() {
+        // export the query method for passing queries to the pool
+    query(text, values) {
+        try {
+            if (values) {
+                console.log('query:', text, values);
+            }
+            else {
+                console.log('query:', text);
+            }
+
+            return this.pool.query(text, values);
+        }
+        catch (err) {
+            console.log('Error With Postgres Query');
+            throw err;
+        }
+    }
+
+    async getLatestAppVersion() {
+        try {
+            let res = this.query(`
+                select app_versions.id, app_versions.app, a.max
+                    from app_versions, (
+                            select app, max(last_dl_attempt)
+                                from app_versions
+                                    where downloaded
+                                    and last_dl_attempt >= $1
+                                    and last_dl_attmept <= $2
+                                    group by app
+                        ) as a
+                    where a.app = app_versions.app
+                    and downloaded
+                    and app_versions.last_dl_attempt >= a.max;
+                    and app_versions.last_dl_attempt >= $1
+                    and app_versions.last_dl_attempt <= $2
+                `,
+                [this.minDate, this.maxDate]
+            )
+            return res.rows;
+        }
+        catch (err) {
+            console.log(`Error retrieving package names from the DB. Error: ${err}`);
+        }
+    }
+
+    async getLatestAppVersion() {
+        try {
+            let res = this.query(`
+                select id, app from app_versions
+                    where downloaded
+                    and last_dl_attempt >= $1
+                    and last_dl_attempt <= $2`,
+                [this.minDate, this.maxDate]
+            )
+            return res.rows;
+        }
+        catch (err) {
+            console.log(`Error retrieving package names from the DB. Error: ${err}`);
+        }
+    }
+
+    async getAppVersionIDs() {
+        if (this.appPackageList.length == 0 && this.useLatestAppVerions) {
+            return await this.getLatestAppVersion();
+        }
+
+        if(this.appPackageList.length == 0 && !this.useLatestAppVerions) {
+            return await this.getAllAppVersions();
+        }
+    }
+
+    async getAPK(apkPath) {
 
     }
 
-    getAPK(apkPath) {
+    async unpackAPK(apk) {
 
     }
 
-    unpackAPK(apk) {
+    async getAPKManifest(apk) {
 
     }
 
-    getAPKManifest(apk) {
+    async analyseApp(mainfest) {
+        return;
+    }
+
+    async storeAnalysisResults(results) {
 
     }
 
-    processAPKManifest(mainfest) {
+    async performAnalysis() {
+        const appPackageNames = await this.getAppVersionIDs();
 
-    }
-
-    storeAnalysisResults(results) {
-
-    }
-
-    performAnalysis() {
-        const appPackageNames = this.getAppPackageNames();
         for(const pName of appPackageNames) {
-            const apk = this.getAPK(pName)
-            this.unpackAPK()
+            const apk = await this.getAPK(pName)
+            await this.unpackAPK()
         }
     }
 
