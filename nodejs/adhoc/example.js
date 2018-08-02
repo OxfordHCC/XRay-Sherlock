fb59932467235971ac5001cd9ba402637709d166
adam slack <adamslack@outlook.com>
2018-08-02 15:15:52 +0100

    example.js

--- nodejs/adhoc/example.js
+++ nodejs/adhoc/example.js
@@ -2,6 +2,27 @@ const Sherlock = require('../sherlock/sherlock');
 
 const regex = /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;
 
+
+/**
+ * Example Ad Hoc
+ *
+ * This class serves as an example of how to extend and implement Sherlock.
+ *
+ * There is essentially only one method that needs to be implemented:
+ *
+ *          async analyseApp(manifest, dexlines, smali) {...}
+ *
+ * For each app that is unpacked and then analysed, this is the method that
+ * is invoked and will then carry out the analysis.
+ *
+ * when searching for specific permissions in the manifest, snippets in the
+ * classes.dex, or hunting through the smali files, this is the method that
+ * you need to implement in order to do that.
+ *
+ * The analysApp method must have 3 parameters, the first is the AndroidManifest.xml
+ * passed into the method as a JSON object. The second is an array of strings, each
+ * string is a line found in the classes.dex file(s). The third is the ...
+ */
 class ExampleAdHoc extends Sherlock {
 
     /**
